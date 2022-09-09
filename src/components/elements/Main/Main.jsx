37fb492b50d56729272a36af741fb4ab435e5628
Сергей Натальин <my.ru.web@gmail.com>
2022-09-09 20:18:05 +0300

    setIsSidbarShow

--- src/components/elements/Main/Main.jsx
+++ src/components/elements/Main/Main.jsx
@@ -3,11 +3,16 @@ import Information from './Information';
 import styles from './Main.module.scss';
 import { DATA } from '../../../data';
 import BottomNavigation from '../../UI/BottomNavigation/BottomNavigation';
+import { useState } from 'react';
 
 const Main = () => {
+	const [isSidbarShow, setIsSidbarShow] = useState(false)
 	return (
 		<div>
-			<Sidebar />
+			<Sidebar
+				isSidbarShow={isSidbarShow}
+				setIsSidbarShow={setIsSidbarShow}
+			/>
 			<div style={{}}>
 				<Information movie={DATA[0]} />
 				<BottomNavigation />
