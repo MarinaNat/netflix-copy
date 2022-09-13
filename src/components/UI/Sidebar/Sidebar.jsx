0be8e476a9e3e4485261782d836edb013dbde3f8
Marina Natalina <Kiska21mk@ya.ru>
2022-09-13 10:25:42 +0300

    button

--- src/components/UI/Sidebar/Sidebar.jsx
+++ src/components/UI/Sidebar/Sidebar.jsx
@@ -4,13 +4,13 @@ const menu = [
 	'Popular', 'TV Shows', 'Films', 'My list'
 ]
 
-const Sidebar = ({ isSidbarShow, setIsSidbarShow }) => {
+const Sidebar = ({ isSidebarShow, setIsSidebarShow }) => {
 	return (
-		<div className={styles.sidebar} style={{ width: isSidbarShow ? '30%' : '15%' }}>
-			<button onClick={() => setIsSidbarShow(!isSidbarShow)}>
-				<i className={`bx bx-${isSidbarShow ? 'x' : 'border-left'}`}></i>
+		<div className={styles.sidebar} style={{ width: isSidebarShow ? '15%' : '10%' }}>
+			<button onClick={() => setIsSidebarShow(!isSidebarShow)}>
+				<i className={`bx bx-${isSidebarShow ? 'x' : 'border-left'}`}></i>
 			</button>
-			<ul className={isSidbarShow ? styles.show : ''}>
+			<ul className={isSidebarShow ? styles.show : ''}>
 				{menu.map(title => (
 					<li key={title}>
 						<a href={title}>{title}</a>
